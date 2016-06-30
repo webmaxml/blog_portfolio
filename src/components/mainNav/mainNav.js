d24135d468c9645076345dedf88b519da7d4abe1
webmaxml <webmaxml@gmail.com>
2016-06-30 15:12:59 +0300

    main nav design

--- src/components/mainNav/mainNav.js
+++ src/components/mainNav/mainNav.js
@@ -10,26 +10,33 @@ class MainNav extends React.Component {
     render() {
         return (
         	<section className="mainNav">
-        		<ul className="mainNav__nav">
-        			<li className="mainNav__nav-item">
-        				<a href="#" className="mainNav__nav-link">Рубрики</a>
-        			</li>
-        			<li className="mainNav__nav-item">
-        				<a href="#" className="mainNav__nav-link">Цитаты</a>
-        			</li>
-        			<li className="mainNav__nav-item">
-        				<a href="#" className="mainNav__nav-link">Контакты</a>
-        			</li>
-        		</ul>
-        		<form className="mainNav__search">
-        			<input type="text" className="mainNav__search-input" placeholder="поиск..."/>
-        			<button className="mainNav__search-btn">
-        				<i className="fa fa-search mainNav__search-icon"></i>
-        			</button>
-        		</form>
-                <button className="mainNav__toggle">
-                    <i className="fa fa-bars mainNav__toggle-icon"></i>
-                </button>
+                <div className="mainNav__wrap">
+            		<ul className="mainNav__nav-line">
+                        <li className="mainNav__nav-wrap">
+                            <a href="#" className="mainNav__nav-link mainNav__nav-link--active">Блог</a>
+                        </li>
+            			<li className="mainNav__nav-wrap">
+            				<a href="#" className="mainNav__nav-link">Рубрики</a>
+            			</li>
+            			<li className="mainNav__nav-wrap">
+            				<a href="#" className="mainNav__nav-link">Цитаты</a>
+            			</li>
+            			<li className="mainNav__nav-wrap">
+            				<a href="#" className="mainNav__nav-link">Контакты</a>
+            			</li>
+            		</ul>
+            		<form className="mainNav__search-line">
+                        <div className="mainNav__input-wrap">
+            		        <input type="text" className="mainNav__search-input" placeholder="поиск..."/>
+                        </div>
+            			<button className="mainNav__search-btn">
+            				<i className="fa fa-search mainNav__search-icon"></i>
+            			</button>
+            		</form>
+                    <button className="mainNav__toggle">
+                        <i className="fa fa-bars mainNav__toggle-icon"></i>
+                    </button>
+                </div>
         	</section>
         );
     }
